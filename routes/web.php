4e99cc74e296d81b1ab33e8057b08b5431eb9323
okdyy75 <okdyy75@gmail.com>
2021-05-26 20:14:26 +0900

    fix password reset routing

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 Route::get('/home', 'HomeController@index')->name('home');
 
 // reset password callback
-Route::get('/reset-password/{token}', 'Auth\ResetPasswordController@resetPassword')
-    ->name('reset-password');
+Route::get('/reset-password/{token}', 'Auth\ResetPasswordController@showResetForm')
+    ->name('password.reset');
+
+// reset password
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset')
+    ->name('password.update');
+
 
 Route::get('/{any}', function () {
     return view('index');
